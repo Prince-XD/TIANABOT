2c1965b5c5b45211c4b05ef858775390d383d491
varun2403 <84765158+varun2403@users.noreply.github.com>
2021-06-11 14:20:51 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -398,7 +398,7 @@ def Source_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "source_":
         query.message.edit_text(
-            text=""" Hi..🤗 I'm *TIANA BOT*
+            text=""" Hi..🤗 I'm *A1 BOT*
                  \nHere is the [Source Code](https://GitHub.com/prince301102/tiana-2.0) .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=False,
